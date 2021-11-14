79f8d27dc61895274d56cb935f268d9fb53945f4
shoppingzh <1049262057@qq.com>
2021-11-14 23:29:53 +0800

    视频捕捉的例子

---
+++ app/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <video
+    ref="video"
+    style="width: 100%; height: 100%;"
+    @loadedmetadata="play"></video>
+</template>
+
+<script>
+import { onMounted, ref } from 'vue'
+export default {
+  setup(props, ctx) {
+    const video = ref(null)
+    const init = async() => {
+      const stream = await navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio: true
+      })
+      console.log(stream)
+      video.value.srcObject = stream
+    }
+    const play = () => {
+      video.value.play()
+    }
+
+    onMounted(() => {
+      init()
+    })
+
+    return {
+      video,
+      play
+    }
+  }
+}
+</script>
+
+
+<style>
+</style>
